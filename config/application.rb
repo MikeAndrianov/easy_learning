765963c5ab1abff922d7d8efd03d9fb07ed4ecb9
Mike Andrianov <mikeandrianov@gmail.com>
2013-11-01 23:48:18 +0300

    added Device for Authentication

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module EasyLearning
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.secret_key_base = '629d9f462ff5acde80b44bb2d495d76052fd79ec1880e796800bcdc28981736bd136aa098ffe578ec9e9f2930fc6dcacb2fce24dbc4fadf84a58ecdf7660094b'
   end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-EasyLearning::Application.config.secret_key_base = '629d9f462ff5acde80b44bb2d495d76052fd79ec1880e796800bcdc28981736bd136aa098ffe578ec9e9f2930fc6dcacb2fce24dbc4fadf84a58ecdf7660094b'
+
