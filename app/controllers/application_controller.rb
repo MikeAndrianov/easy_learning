bf5a09367173208c786a1e238ce85a2c9110d31c
Mike Andrianov <mikeandrianov@gmail.com>
2013-11-12 02:16:41 +0300

    Added ability for changing Title depending on the current page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_app_name
+
+  protected
+
+  def set_app_name
+    @app_name = "EasyLearing"
+  end
 
 end
