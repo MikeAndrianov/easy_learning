f02a8215c630d6bd04e839657049c969b4b6ca36
Mike Andrianov <mikeandrianov@gmail.com>
2014-02-16 17:09:16 +0300

    Customized errors presentation on forms

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 EasyLearning::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+  "<div class='control-group error'>#{html_tag}</div>".html_safe
+end
