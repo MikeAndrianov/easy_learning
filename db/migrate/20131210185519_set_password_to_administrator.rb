1e8389a68eba04fafa7e067b07eb886201e198a7
Ilya Berinchik <ilyabyar@gmail.com>
2014-03-03 00:42:23 +0300

    removed unuseful migration set_password_to_administrator

--- db/migrate/20131210185519_set_password_to_administrator.rb
+++
@@ -1,8 +0,0 @@
-class SetPasswordToAdministrator < ActiveRecord::Migration
-  def up
-    admin = ::User.find_or_create_by_email_and_name("admin@email.com", "Admin")
-    admin.password = admin.password_confirmation = "easylearning"
-    admin.role = "Administrator"
-    admin.save!
-  end
-end
