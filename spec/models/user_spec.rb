93872ed0f7646691bf74b93facc7d9828780a18c
pavlik4k <pavlik4k@gmail.com>
2014-03-06 13:24:06 +0300

    Add methods specs for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,23 @@ describe User do
 	describe "factories" do
 		it { FactoryGirl.create(:student).should be_valid }
 		it { FactoryGirl.build(:student, email: nil).should_not be_valid }
+		it { FactoryGirl.build(:student, name: nil).should_not be_valid }
 		it { FactoryGirl.create(:student).type.should eq("Student") }
 		it { FactoryGirl.create(:lecturer).type.should eq("Lecturer") }
 		it { FactoryGirl.create(:admin).type.should eq("Admin") }
 	end
+
+	describe "type methods" do
+		before do
+		  @admin = FactoryGirl.create(:admin)
+		  @lecturer = FactoryGirl.create(:lecturer)
+		  @student = FactoryGirl.create(:student)
+		end
+		it { @admin.admin?.should eq(true) }
+		it { @lecturer.lecturer?.should eq(true) }
+		it { @student.student?.should eq(true) }
+		it { @student.lecturer?.should eq(false) }
+		it { @student.admin?.should eq(false) }
+	end
+
 end
