79c998aa0d6d0eadf0065ccb3c970408d028bc41
Mike Andrianov <mikeandrianov@gmail.com>
2014-03-18 01:44:58 +0300

    redirection fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,7 @@ class HomeController < ApplicationController
   before_filter :set_the_header
   
   def index
-    # @users = User.all
-
-    # redirect_to user_home_index_path if current_user
+    redirect_to account_url if current_user
   end
 
   [:about, :contact, :services, :portfolio].each do |action_name| 
