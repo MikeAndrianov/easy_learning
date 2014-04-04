aa208d8c63e1d1ba06a3ee2e9b5e8aeee9133c39
Marina Harlach <marina.harlach@gmail.com>
2014-04-04 23:15:16 +0300

    Show faculty name

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -7,6 +7,10 @@ module HomeHelper
     @resource ||= User.new
   end
 
+  def faculty_name
+  	Faculty.find_by(:subdomain => request.subdomain).try(:name)
+  end
+
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
