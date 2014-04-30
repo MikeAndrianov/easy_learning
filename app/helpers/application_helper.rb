fa554c4f10a384a758ea48b780eab370a8c23502
marat-chardymov <marat.chardymov@gmail.com>
2014-04-30 23:33:53 +0300

    subject menu active li highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -48,4 +48,13 @@ module ApplicationHelper
     res.html_safe
   end
 
+  #used to add css class 'active' to current nav list li element
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+
 end
